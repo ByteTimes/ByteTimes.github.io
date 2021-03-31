554f5c2cfca12251c611564301cfd6a451b518d7
SavileLee <58343733+SavileLee@users.noreply.github.com>
2021-03-31 15:39:00 +0800

    Update OSNET.sh

--- OSNET.sh
+++ OSNET.sh
@@ -325,7 +325,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://github.com/MoeClub/MoeClub.github.io/raw/master/lib/wget_udeb_amd64.tar.gz';
+  export SSL_SUPPORT='https://github.com/SavileLee/SavileLee.github.io/raw/html/OSNET/wget_udeb_amd64.tar.gz';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
